d5e3bb52b59c1ca86777d9e5266d82c4f150e116
BiswajitHemram <biswajithemram753@gmail.com>
2023-08-19 19:14:15 +0530

    add function blackColorBtn and decalare variable sketch globally

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+const sketch = document.querySelector('.sketchBox') //declaring sketch global
+// defaultGridSize() work is generate default  div box for grid of 16*16
 function defaultGridSize(){
-    const sketch = document.querySelector('.sketchBox')
     console.log(sketch)
-    for(let i=0; i<16; i++){
+    for(let i=0; i<256; i++){
         const box = document.createElement('div')
         box.classList.add('items')
         sketch.appendChild(box)
@@ -11,5 +12,16 @@ function defaultGridSize(){
     sketch.style.setProperty('--column-count', columnCount)
     sketch.style.setProperty('--row-count', rowCount)
 }
-
 window.addEventListener('load',defaultGridSize)
+
+// when ever user click on black button than mouse over on grid change into black 
+const blackColorBtn = ()=>{
+        sketch.addEventListener('mouseover',function(event){
+            if(event.target.classList.contains('items')){
+                event.target.style.backgroundColor = 'black';
+            }
+        })
+    }
+const blackBtn = document.querySelector('.btn-black')
+blackBtn.addEventListener('click',blackColorBtn)
+
